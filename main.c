e7264acb25df074623016e0c217583ef70a5c733
Mehmet Berkay Alçın <119623737+mhmtbrky35@users.noreply.github.com>
2023-12-27 04:25:18 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FRAME_LENGTH 100 // Defining frame length
+#define FRAME_WIDTH 100  // Defining frame width
+
+char frame[FRAME_LENGTH][FRAME_WIDTH]; // Initializing frame
+
+int main()
+{
+
+    return 0;
+}
